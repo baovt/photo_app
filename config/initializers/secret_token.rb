f82d4dd1162fddaf330830d02eb44731b639ac30
Bao Vuong <bao@mars.(none)>
2011-06-21 23:57:39 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoApp::Application.config.secret_token = 'dd37a61836e0daca7ac6133748c8ff2129b487087ed428a9e778cad3b0ef0615dac17cb1afec5d9565cd7b600e82f737d2eba49fbb76d8b6dbfd215223d0abb9'
